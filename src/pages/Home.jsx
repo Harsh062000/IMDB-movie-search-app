1fe109f808771e6f7c3afc01014db63ab861a892
Harsh Gohil <gohil.harsh06@gmail.com>
2025-04-19 20:41:23 +0530

    Added movie card UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import MovieCard from '../components/MovieCard/MovieCard';
+import './Home.css';
 
 function Home() {
+
+       const Movie = {
+              "Title": "Harry Doright's Prelude to Hell",
+              "Year": "2008",
+              "Type": "movie",
+              "Poster": "https://m.media-amazon.com/images/M/MV5BZGUyNzU0OTgtZTM0NS00MWY0LWJhZjAtYzM5ZmZlY2IwNzhlXkEyXkFqcGdeQXVyNDU4ODIyNjU@._V1_SX300.jpg"
+       }
        return (
               <>
                      {/* navbar */}
                      {/* Movie list */}
                      {/* pagination buttons */}
-                     Home
+
+                     <div className='movie-card-wrapper'>
+                            <MovieCard
+                                   {...Movie}
+                            />
+                            <MovieCard
+                                   {...Movie}
+                            />
+                            <MovieCard
+                                   {...Movie}
+                            />
+                     </div>
+
               </>
        )
 }
